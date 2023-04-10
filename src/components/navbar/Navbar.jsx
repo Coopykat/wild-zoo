38a765c305215a2f9dc1c4da6d90d51704adb285
Coopykat <kathycamazon@gmail.com>
2023-04-10 18:40:19 +0200

    menu links ok

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,7 +6,11 @@ function Navbar() {
     return (
         <div className="Navbar">
             <a href="/" ><img className="logo" src={logo} alt="zoo logo" /></a>
-
+            <div className="navigation" >
+                <a className="navigation-link" href="/">NOS ANIMAUX</a>
+                <a className="navigation-link" href="/">À PROPOS</a>
+                <a className="navigation-link" href="/">CONTACT</a>
+            </div>
 
         </div>
     )
